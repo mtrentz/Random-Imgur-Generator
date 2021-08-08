7667e516ec0c22379a0465e7ee685bee169d7a07
mtrentz <mateus.trentz@gmail.com>
2021-08-08 15:12:41 -0300

    cria a pasta imgs no inicio

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const imageDir string = "imgs"
+
+func init() {
+	if _, err := os.Stat(imageDir); os.IsNotExist(err) {
+		err := os.Mkdir(imageDir, 0700)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
 
 func main() {
 
 	baseUrl := "https://i.imgur.com/"
 
-	for i := 0; i < 25; i++ {
-		// Gera o codigo do imgur
+	// Tenta N vezes gerar um url válido. Pode ser que nao consiga nenhum.
+	for i := 0; i < 10; i++ {
+		// Gera o codigo do imgur, o numero é referete a quantidade de digitos random no fim do link
+		// links com 5 digitos random geralmente são imagens mais antigas. Com 6 já são mais novas.
+		// Porém, com 6 digitos, a grande maioria dos chutes serão 404.
 		code := ImgurCodeGenerator(5)
 		// Coloca um .png pra garantir que o site vai abrir só a imagem caso existir
 		// Isso funciona mesmo se o original no site seja jpeg, e ele salva .png mesmo sendo jpeg e pelo visto funciona
@@ -16,7 +33,7 @@ func main() {
 		url := baseUrl + imageName
 		fmt.Printf("%d - %s \n", i+1, url)
 		// Salva o arquivo caso seja mesmo uma img. Mesmo se for um jpeg vai salvar
-		filePath := "imgs/" + imageName
+		filePath := imageDir + "/" + imageName
 		DownloadImage(filePath, url)
 	}
 }
