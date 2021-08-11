e799a1a65aec1ee2ffa27de8e4094f31061fedd9
mtrentz <mateus.trentz@gmail.com>
2021-08-10 22:47:21 -0300

    funcionando

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 
 const imageDir string = "imgs"
 
-var c chan int
-
 func init() {
 	// Caso não exista, cria a página onde vou guardar as imgs
 	if _, err := os.Stat(imageDir); os.IsNotExist(err) {
@@ -27,7 +25,6 @@ func makeRequest(codeLen int, c chan int) {
 	// links com 5 digitos random geralmente são imagens mais antigas. Com 6 já são mais novas.
 	// Porém, com 6 digitos, a grande maioria dos chutes serão 404.
 	code := ImgurCodeGenerator(codeLen)
-	fmt.Println(code)
 
 	// Coloca um .png pra garantir que o site vai abrir só a imagem caso existir
 	// Isso funciona mesmo se o original no site seja jpeg, e ele salva .png mesmo sendo jpeg e pelo visto funciona
@@ -39,13 +36,12 @@ func makeRequest(codeLen int, c chan int) {
 	// Salva o arquivo caso seja mesmo uma img. Mesmo se for um jpeg vai salvar
 	filePath := imageDir + "/" + imageName
 
-	// complete, err := DownloadImage(filePath, url)
-	fmt.Println(filePath + url)
-	complete := false
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	c <- 0
-	// }
+	complete, err := DownloadImage(filePath, url)
+
+	if err != nil {
+		fmt.Println(err)
+		c <- 0
+	}
 
 	if complete {
 		c <- 1
@@ -59,6 +55,7 @@ func main() {
 	// Quantidade de imagens que quero baixar
 	imgsWanted := 10
 	counter := 0
+	c := make(chan int)
 
 	// Roda por n vezes,
 	for i := 0; i < 500; i++ {
